546e95f5242bbbe03359bf34d3c60ba9f34f84dc
jfernandes2001 <32169001+jfernandes2001@users.noreply.github.com>
2017-11-09 15:14:10 +0000

    Add files via upload

---
+++ operaçoes.java
@@ -0,0 +1,12 @@
+package primeirascontas;
+
+public class operaçoes {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.printf("%d\n", 4+3*(15-2));
+		System.out.printf("%f\n", 40.0/9.0);
+		System.out.printf("%d", 40%3);
+	}
+
+}
