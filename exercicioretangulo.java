a2ecf77009f0c01bfed39386179a9c7dff9dccbf
jfernandes2001 <32169001+jfernandes2001@users.noreply.github.com>
2017-11-09 15:26:58 +0000

    Add files via upload

---
+++ exercicioretangulo.java
@@ -0,0 +1,16 @@
+package retangulo;
+
+public class exercicioretangulo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		public static void main(String[] args) {
+			 Scanner s = new Scanner(System.in);
+			 System.out.println("Qual o Comprimento?");
+			 int comp = s.nextInt();
+			 System.out.println("Qual a Largura?");
+			 int larg = s.nextInt();
+			 System.out.println(comp*larg);
+	}
+
+}
