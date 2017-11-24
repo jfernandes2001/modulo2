c06d75989990f34abf2b5cd62b266eb2952929b7
jfernandes2001 <32169001+jfernandes2001@users.noreply.github.com>
2017-11-24 15:02:35 +0000

    Add files via upload

---
+++ bem_vindo_ao_java.java
@@ -0,0 +1,14 @@
+package bemvindoaojava;
+
+public class bem_vindo_ao_java {
+
+	public static void main(String[] args) {
+		final String mensagem = "Hoje é sexta feira";
+		for(int I=1 ; I<=5 ; I++)
+			System.out.println(mensagem);
+		
+			
+
+	}
+
+}
