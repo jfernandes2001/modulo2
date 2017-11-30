a803ab1823542b144c70c66578929f5972c18b03
jfernandes2001 <32169001+jfernandes2001@users.noreply.github.com>
2017-11-30 17:02:44 +0000

    Add files via upload

---
+++ diadasbruxas.java
@@ -0,0 +1,22 @@
+package Diadasbruxas;
+
+import java.util.Scanner;
+
+public class diadasbruxas {
+
+   public static void main(String[] args) {
+		Scanner	dados = new Scanner (System.in);
+			String C = "";
+			//Variaveis
+			System.out.println("...:Dia das Bruxas:...");
+			System.out.println("Doce ou Travessura? ");
+			C = dados.nextLine();
+			System.out.print("o Utilizador escolheu " + C + ".");
+		}
+
+	}	
+		
+
+	
+
+
